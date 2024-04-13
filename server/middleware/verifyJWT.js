a3fd89cf60c6dfcbd14b728f2ad0aaa4f344de6e
Imran-548 <imranmlk14@gmail.com>
2024-04-13 19:07:25 -0400

    authHeader

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -3,12 +3,13 @@ require("dotenv").config();
 const secret = require("../config/secret");
 
 const verifyJWT = (req, res, next) => {
-  const token = req.headers.authorization;
-  if (!token)
+  const authHeader = req.headers.authorization;
+  if (!authHeader)
     return res
       .status(401)
       .send("Access Denied, authentication headers required");
 
+  const token = authHeader.split(" ")[1];
   jwt.verify(token, secret, (err, decoded) => {
     if (err) {
       console.log(err);
